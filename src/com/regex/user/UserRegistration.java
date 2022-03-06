9064133384ee7522bc72a7ef9ed0f8c3786c7872
17harshu <harshalanandas17@gmail.com>
2022-03-06 17:52:34 +0530

    added: user password with uppercase

--- src/com/regex/user/UserRegistration.java
+++ src/com/regex/user/UserRegistration.java
@@ -45,10 +45,21 @@ public class UserRegistration {
 			System.out.println("Invalid Mobile");
 	}
 
+	public static void password() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\nEnter the Password");
+		String p = sc.nextLine();
+		if (Pattern.matches("^(?=.*[a-z])\"+\"(?=.*[A-Z]){8,}$", p) == true)// should have one upper case
+			System.out.println("Valid Password");
+		else
+			System.out.println("Invalid Password");
+	}
+
 	public static void main(String[] args) {
 		// firstName();
 		// lastName();
 		// email();
-		mobile();
+		// mobile();
+		password();
 	}
 }
